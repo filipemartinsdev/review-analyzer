e3cdc67a61c61597cafcae987f6b31075df0959f
filipemartinsdev <dev.filipemartins@gmail.com>
2025-09-13 13:34:42 -0300

    organizando classe Analyzer

--- src/main/java/com/reviewanalyzer/nlp/Analyzer.java
+++ src/main/java/com/reviewanalyzer/nlp/Analyzer.java
@@ -1,7 +1,9 @@
 package com.reviewanalyzer.nlp;
 
-public class Analyzer {
-    public static void main(String[] args) {
+import opennlp.tools.sentdetect.SentenceModel;
 
+public class Analyzer {
+    public static Sentiment analyzeSentiment(String text){
+        return Sentiment.NEUTRAL;
     }
 }
--- src/main/java/com/reviewanalyzer/service/ReviewAnalyzer.java
+++ src/main/java/com/reviewanalyzer/service/ReviewAnalyzer.java
@@ -1,9 +1,10 @@
 package com.reviewanalyzer.service;
 
+import com.reviewanalyzer.nlp.Analyzer;
 import com.reviewanalyzer.nlp.Sentiment;
 
 public class ReviewAnalyzer {
     public static Sentiment getSentiment(String review){
-        return Sentiment.NEUTRAL;
+        return Analyzer.analyzeSentiment(review);
     }
 }
