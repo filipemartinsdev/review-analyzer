684aa0477c86135f4c87c52b04747ec5de0c41ea
filipemartinsdev <dev.filipemartins@gmail.com>
2025-09-13 13:54:32 -0300

    modelando respose da API

--- src/main/java/com/reviewanalyzer/model/ReviewResponse.java
+++ src/main/java/com/reviewanalyzer/model/ReviewResponse.java
@@ -1,9 +1,14 @@
 package com.reviewanalyzer.model;
 
 public class ReviewResponse {
-    public ReviewResponse(){}
+    public int fr_percent_positive;
+    public int fr_percent_neutral;
+    public int fr_percent_negative;
 
-    public int positivePercent;
-    public int neutralPercent;
-    public int negativePercent;
+    public int fi_positive;
+    public int fi_neutral;
+    public int fi_negative;
+
+    public ReviewResponse(){}
 }
+
