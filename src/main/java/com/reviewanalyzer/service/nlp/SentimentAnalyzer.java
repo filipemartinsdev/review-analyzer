05697294e64d5b76aa13daa5e1eddaf8765d24b5
filipemartinsdev <dev.filipemartins@gmail.com>
2025-09-14 16:09:59 -0300

    coloca TODOS

--- src/main/java/com/reviewanalyzer/service/nlp/SentimentAnalyzer.java
+++ src/main/java/com/reviewanalyzer/service/nlp/SentimentAnalyzer.java
@@ -14,14 +14,17 @@ public class SentimentAnalyzer {
     }
 
     private static String[] tokenizer(String text){
+        // TODO: IMPLEMENTAR METODO
         return new String[]{};
     }
 
     private static String[] removeStopWords(String[] textTokens){
+        // TODO: IMPLEMENTAR METODO
         return new String[]{};
     }
 
     private static Sentiment classify(String[] textTokens){
+        // TODO: IMPLEMENTAR METODO
         return Sentiment.NEUTRAL;
     }
 }
