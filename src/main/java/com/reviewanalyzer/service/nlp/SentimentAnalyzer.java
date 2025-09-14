d6c92b54ff652715a3ba710d50b2354a6d999a14
filipemartinsdev <dev.filipemartins@gmail.com>
2025-09-14 18:58:28 -0300

    teste

--- src/main/java/com/reviewanalyzer/service/nlp/SentimentAnalyzer.java
+++ src/main/java/com/reviewanalyzer/service/nlp/SentimentAnalyzer.java
@@ -27,4 +27,46 @@ public class SentimentAnalyzer {
         // TODO: IMPLEMENTAR METODO
         return Sentiment.NEUTRAL;
     }
+
+    private static String[] testDataTraine = {
+            // Positivos
+            "positive\tÓtimo produto!",
+            "positive\tGostei demais!",
+            "positive\tUma belezinha",
+            "positive\tChegou bem antes do prazo!",
+            "positive\tExcelente qualidade",
+            "positive\tRecomendo muito",
+            "positive\tSuperou minhas expectativas",
+            "positive\tAmei o produto",
+            "positive\tMuito bom mesmo",
+            "positive\tPerfeito para o que eu precisava",
+
+            // Neutros
+            "neutral\tQualidade mediana",
+            "neutral\tEntrega na data prometida",
+            "neutral\tNada demais",
+            "neutral\tNada mal",
+            "neutral\tProduto comum",
+            "neutral\tAtendeu as expectativas",
+            "neutral\tNormal",
+            "neutral\tOk, sem mais",
+
+            // Negativos
+            "negative\tPéssima qualidade!",
+            "negative\tChegou muito depois do prazo",
+            "negative\tEmbalagem veio com lacre violado!",
+            "negative\tProduto frágil",
+            "negative\tNão recomendo",
+            "negative\tQualidade horrível",
+            "negative\tDinheiro jogado fora",
+            "negative\tMuito ruim",
+            "negative\tDecepcionante",
+            "negative\tPior compra que já fiz"
+    };
+
+    private static String[] STOP_WORDS = {
+            "o", "a", "um", "e", "ou", "mas", "é", "no", "na", "para", "de",
+            "do", "com", "por", "e", "são", "era", "foi", "ser", "sendo", "tem",
+            "possui", "contém", "do", "faz", "fez", "fará", "faria"
+    };
 }
