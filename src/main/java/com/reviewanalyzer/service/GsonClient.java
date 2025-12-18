5555e5e7b7fd14c8dd12c1b38117095ca5d24454
filipemartinsdev <dev.filipemartins@gmail.com>
2025-12-18 10:27:22 -0300

    feat(service): create Gson client class as JsonParser implementation

--- src/main/java/com/reviewanalyzer/service/GsonClient.java
+++ src/main/java/com/reviewanalyzer/service/GsonClient.java
@@ -1,4 +1,31 @@
 package com.reviewanalyzer.service;
 
-public class GsonClient {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class GsonClient implements JsonParser {
+    private final Gson gson;
+
+    public GsonClient(){
+        this.gson = new Gson();
+    }
+
+    @Override
+    public String toJson(Object obj) {
+        return this.gson.toJson(obj);
+    }
+
+    @Override
+    public <T> T fromJson(String json, Class<T> clazz) {
+        return this.gson.fromJson(json, clazz);
+    }
+
+    @Override
+    public <T> List<T> fromJsonList(String json, Class<T> elementClass){
+        Type listType = TypeToken.getParameterized(List.class, elementClass).getType();
+        return this.gson.fromJson(json, listType);
+    }
 }
